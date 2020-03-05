6756a28aa519077ac3c25b852d630fe0a5321698
Rody Davis <rody@rive.app>
2020-03-04 19:18:21 -0800

    dynamic page route

---
+++ lib/screens/2.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Screen2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Screen 2')),
+    );
+  }
+}
